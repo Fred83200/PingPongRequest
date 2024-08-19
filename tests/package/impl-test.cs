6f1b28cebfd911c79dd98ba931f4b344f24d117f
Frederic Jonquieres <fjonquieres83@gmail.com>
2024-08-19 23:11:29 +0100

    Add project files.

---
+++ tests/package/impl-test.cs
@@ -0,0 +1,21 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+public class PingPongTest
+{
+    [Fact]
+    public async Task Ping_Should_Return_Pong()
+    {
+        await using var application = new Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactory<Program>();
+        var client = application.CreateClient();
+
+        var response = await client.PostAsync("/ping", null);
+
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Equal("Pong", content);
+    }
+}
