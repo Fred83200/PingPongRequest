0481deaa4208fcf36263ca1aea1ca1f90131a2b8
Frederic Jonquieres <fjonquieres83@gmail.com>
2024-08-20 13:54:16 +0100

    Last clean up / Organising file to match required structure

---
+++ src/package/impl.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PingPong
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            app.MapPost("/ping", () => Results.Ok("pong"));
+            app.Run();
+        }
+    }
+}
